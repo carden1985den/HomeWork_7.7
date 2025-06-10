20c1ffae2df878d55ae0124c22d64c9797a6e447
denis.tsaregorodtsev <carden1985@gmail.com>
2025-06-10 12:33:04 +0400

    Create Project "HomeWork_7.7"

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿abstract class Delivery
+{
+    public string Address;
+}
+
+class HomeDelivery : Delivery
+{
+    /* 
+    доставка на дом. Этот тип будет подразумевать наличие курьера или передачу курьерской компании, 
+    в нём будет располагаться своя, отдельная от прочих типов доставки, логика
+     
+     */
+}
+
+class PickPointDelivery : Delivery
+{
+    /*
+    PickPointDelivery — доставка в пункт выдачи. Здесь будет храниться какая-то ещё логика,
+    необходимая для процесса доставки в пункт выдачи, например, хранение компании и точки выдачи, 
+    а также какой-то ещё информации  
+    */
+}
+
+class ShopDelivery : Delivery
+{
+    /*
+    доставка в розничный магазин. Эта доставка может выполняться внутренними средствами 
+    компании и совсем не требует работы с «внешними» элементами
+    */
+}
+
+class Order<TDelivery, TStruct> where TDelivery : Delivery
+{
+    public TDelivery Delivery;
+
+    public int Number;
+
+    public string Description;
+
+    public void DisplayAddress()
+    {
+        Console.WriteLine(Delivery.Address);
+    }
+
+    // ... Другие поля
+}
+/*
+ * 
