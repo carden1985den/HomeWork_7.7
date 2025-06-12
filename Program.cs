404a85be6dc4c18ff7e11788a6ebd6cc828e2d96
denis.tsaregorodtsev <carden1985@gmail.com>
2025-06-12 07:35:32 +0400

    1

--- Program.cs
+++ Program.cs
@@ -1,30 +1,67 @@
-﻿abstract class Weapon
+﻿enum OrderItem
 {
-    abstract public void Fire();
+    food = 0,
+    clothes,
+    perfumery,
+    chemistry,
+    trinkets
 }
 
-class Gun:Weapon
+class Order //<TPhone>
 {
-    public override void Fire()
+    //элементы заказа
+    public OrderItem[] orderItem;
+    //номер заказа
+    public int _orderNumber;
+    private string _recipientAddress;
+
+    
+    public int AssignOrderNumber(ref int orderNumber)
     {
-        Console.WriteLine("Gso");
+        _orderNumber = orderNumber + 1;
+        return _orderNumber;
     }
-}
-
-class Player
-{
-    public void Fire(Weapon weapon)
+    
+    //адрес доставки
+    public string RecipientAddress
     {
-        weapon.Fire();
+        get {
+            return _recipientAddress;
+        }
+        set
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                _recipientAddress = value;
+            }
+        }
     }
+
+    //номер телефона получателя, может быть и 89121234556 
+    public string RecipientPhone {get;set;}
+
+    // заказ надо собрать
+    public void PackOrder(params OrderItem[] item)
+    {
+        this.orderItem = item;
+    }
+    
+    //Отобразить детали по заказу
+    public void DisplayOrder()
+    {
+        Console.WriteLine("Заказ номер\t\t\t{0}\nАдрес доставки\t\t{1}\nНомер телефона получателя {2}", _orderNumber, RecipientAddress, RecipientPhone);
+    }
+    // отправить курьером
 }
 
-class MyApp
+class Programm
 {
     static void Main()
     {
-        Gun gun = new Gun();        
-        Player player = new Player();
-        player.Fire(gun);
+        int orderNumber = 0000;
+        var bag1 = new Order();
+        bag1.PackOrder(OrderItem.food, OrderItem.chemistry);
+        //List<OrderItem> itemCollection = new List<OrderItem>();
+        bag1.DisplayOrder();
     }
 }
